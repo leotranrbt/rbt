b998c17c58b18d71d66ef126642577aa9117b127
Leo Tran <leopoldtran8899@outlook.com>
2025-01-13 16:50:02 +1100

    Fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/rbt",
+  assetPrefix: isProd ? '/rbt' : '',
+  basePath: isProd ? '/rbt' : '',
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
